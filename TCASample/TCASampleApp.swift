b9a57e91ae7a7a5d423c647875ff858fe2c51916
Jeong <jeong123wj@gmail.com>
2024-10-26 16:38:15 +0900

    Initial Commit

---
+++ TCASample/TCASampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCASampleApp.swift
+//  TCASample
+//
+//  Created by Woojin on 10/26/24.
+//
+
+import SwiftUI
+
+@main
+struct TCASampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
